31766b9b081d2413baded81a9b02503ab24fa8f3
wnccys <ipooo346@gmail.com>
2024-06-03 23:42:40 -0300

    feat: sets InterpretResult enum and vm new/interpret functions implementation

--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -1,1 +1,32 @@
+use crate::chunk::*;
 
+pub enum InterpretResult {
+    Ok,
+    CompileError,
+    RuntimeError,
+}
+
+pub struct Vm<'a> {
+    chunk: Option<&'a Chunk<'a>>,
+    ip: Option<&'a Vec<&'a OpCode<'a>>>,
+}
+
+// REVIEW set correct VM structure
+// static mut VM: Vm = Vm::new();
+
+impl<'a> Vm<'a> {
+    pub fn new() -> Self {
+        Vm {
+            chunk: None,
+            ip: None,
+        }
+    }
+
+    pub fn interpret(&mut self, chunk: &'a Chunk) {
+        self.chunk = Some(chunk);
+        self.ip = Some(&chunk.code);
+        // self.run()
+    }
+
+    //    fn run(&mut self) -> InterpretResult {}
+}

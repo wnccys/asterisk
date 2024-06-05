d21ba0d07175f9b1f61e85d11f64ee471df38fd7
wnccys <ipooo346@gmail.com>
2024-06-04 22:09:32 -0300

    ref: removes unused match arm

--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -33,20 +33,17 @@ impl<'a> Vm<'a> {
     fn run(&self) -> InterpretResult {
         let mut result = InterpretResult::CompileError;
 
-        match self.ip {
-            Some(vec) => {
-                for &opcode in vec.iter() {
-                    match opcode {
-                        OpCode::OpReturn => result = InterpretResult::Ok,
-                        OpCode::OpConstant(index) => {
-                            let constant = self.chunk.unwrap().constants[**index];
-                            print_value(constant);
-                            result = InterpretResult::Ok
-                        }
+        if let Some(vec) = self.ip {
+            for &opcode in vec.iter() {
+                match opcode {
+                    OpCode::OpReturn => result = InterpretResult::Ok,
+                    OpCode::OpConstant(index) => {
+                        let constant = self.chunk.unwrap().constants[**index];
+                        print_value(constant);
+                        result = InterpretResult::Ok
                     }
                 }
             }
-            None => result = InterpretResult::RuntimeError,
         }
 
         result

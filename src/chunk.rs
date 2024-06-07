b0d735701db0524ba41c5eb236e97fbe800cbe89
wnccys <ipooo346@gmail.com>
2024-06-07 18:20:16 -0300

    feat: removes static references idea     derives clone trait, implements custom_copy     for proper VM value handling and sets negate     method correctly

--- src/chunk.rs
+++ src/chunk.rs
@@ -4,17 +4,18 @@ pub enum OpCode<'a> {
     OpNegate,
 }
 
-pub enum Value<'a> {
-    Float(&'a f32),
+#[derive(Clone)]
+pub enum Value {
+    Float(f32),
 }
 
 pub struct Chunk<'a> {
     pub count: usize,
     pub code: Vec<&'a OpCode<'a>>,
-    pub stack: Vec<&'a Value<'a>>,
+    pub stack: Vec<Value>,
     pub stack_top: usize,
     pub constant_count: usize,
-    pub constants: Vec<&'a Value<'a>>,
+    pub constants: Vec<Value>,
     pub lines: Vec<i32>,
 }
 
@@ -37,18 +38,18 @@ impl<'a> Chunk<'a> {
         self.lines.push(line);
     }
 
-    pub fn write_constant(&mut self, value: &'a Value) -> usize {
+    pub fn write_constant(&mut self, value: Value) -> usize {
         self.constant_count += 1;
         self.constants.push(value);
         self.constants.len() - 1
     }
 
-    pub fn push_stack(&mut self, value: &'a Value) {
+    pub fn push_stack(&mut self, value: Value) {
         self.stack.push(value);
         self.stack_top += 1;
     }
 
-    pub fn pop_stack(&mut self) -> &Value {
+    pub fn pop_stack(&mut self) -> Value {
         self.stack_top -= 1;
 
         return self.stack.pop().unwrap() 
@@ -56,18 +57,18 @@ impl<'a> Chunk<'a> {
 
 }
 
-pub trait ValueOperations {
-    fn negate(&self) -> Value<'_>;
-}
+impl Copy for Value {}
 
-impl<'a> ValueOperations for Value<'a> {
-    fn negate(&self) -> Value {
+impl Value {
+     pub fn negate(&self) -> Value {
         match self {
-            Value::Float(value) => {
-                let result = (*value * -1.0) as f32;
-                Value::Float(Box::leak(Box::new(result)))
-            },
+            Value::Float(value) => Value::Float(-value),
             _ => panic!("operation not allowed for this variant"),
         }
-    }
+     }
+    // pub fn custom_copy(&self) -> Self {
+    //     match self {
+    //         Value::Float(value) => Value::Float(*value),
+    //     }
+    // }
 }

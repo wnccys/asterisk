6a680ed01a02f89df3bd7970dcb0a28fbfff0305
wnccys <ipooo346@gmail.com>
2024-06-08 14:55:06 -0300

    fix: div op works properly

--- src/value.rs
+++ src/value.rs
@@ -8,15 +8,7 @@ pub enum Value {
 
 impl Copy for Value {}
 
-impl Value {
-     pub fn negate(&self) -> Value {
-        match self {
-            Value::Float(value) => Value::Float(-value),
-            Value::Int(value) => Value::Int(-value),
-            _ => panic!("operation not allowed for this variant"),
-        }
-     }
-}
+impl Value {}
 
 impl Add for Value {
     type Output = Value;
@@ -25,18 +17,6 @@ impl Add for Value {
         match(self, other) {
             (Value::Float(a), Value::Float(b)) => Value::Float(a+b),
             (Value::Int(a), Value::Int(b)) => Value::Int(a+b),
-            _ => panic!("different type operations are now allowed"),
-        }
-    }
-}
-
-impl Sub for Value {
-    type Output = Value;
-
-    fn sub(self, other: Value) -> Value {
-        match (self, other) {
-            (Value::Float(a), Value::Float(b)) => Value::Float(a-b),
-            (Value::Int(a), Value::Int(b)) => Value::Int(a-b),
             _ => panic!("different type operations are now allowed"),
         }
     }
@@ -59,8 +39,8 @@ impl Div for Value {
 
     fn div(self, other: Value) -> Value {
         match (self, other) {
-            (Value::Float(a), Value::Float(b)) => Value::Float(a+b),
-            (Value::Int(a), Value::Int(b)) => Value::Int(a+b),
+            (Value::Float(a), Value::Float(b)) => Value::Float(a/b),
+            (Value::Int(a), Value::Int(b)) => Value::Int(a/b),
             _ => panic!("different type operations are now allowed"),
         }
     }

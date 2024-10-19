13d3946a14ba7ac8eb81b7667e0924f3a4bad5f6
wnccys <ipooo346@gmail.com>
2024-10-19 10:10:54 -0300

    feat: sets basic model for hashtable representation and hashing

--- src/types.rs
+++ src/types.rs
@@ -1,12 +1,56 @@
+use std::rc::Rc;
+
 use crate::value::Value;
+pub type Hash = u32;
+
 
 struct Entry {
     key: Vec<char>,
     value: Value
 }
 
-pub struct Table<'a> {
+pub struct Table {
     count: i64,
-    capacity: i64,
-    entries: &'a Entry,
+    entries: Option<Vec<Rc<Entry>>>,
+}
+
+impl Default for Table {
+    fn default() -> Self {
+       Self {
+            count: 0,
+            entries: None
+       } 
+    }
+}
+
+impl Table {
+    fn set(&mut self, key: Vec<char>, value: Value) -> bool {
+        true
+    }
+}
+
+pub fn hash_string(key: &mut Vec<char>) -> Hash {
+    let mut hash: Hash = 2166136261;
+
+    for i in 0..key.len() {
+        println!("i is {i}, key is: {}", key[i]);
+        hash ^= key[i].to_digit(36).unwrap();
+        hash = hash.wrapping_mul(16777619);
+    }
+
+    hash
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hashing() {
+        let mut word = vec!{'n', 'u', 'm'};
+        println!("hash: {}", hash_string(&mut word));
+
+        let mut word = vec!{'z', 'a', 'z', 'a'};
+        println!("hash: {}", hash_string(&mut word));
+    }
 }

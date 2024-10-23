9c6986546844a58f94b8c2cebf6cdd047beb8535
wnccys <ipooo346@gmail.com>
2024-10-23 20:09:57 -0300

    feat: setting up set and find_entry methods

--- src/types.rs
+++ src/types.rs
@@ -1,42 +1,56 @@
-use std::{rc::Rc};
+use std::{fmt::Error, rc::Rc};
 
 use crate::value::Value;
 pub type Hash = u32;
-const MAX_LOAD: f32 = 0.75;
 
 struct Entry {
     key: Vec<char>,
-    value: Value
+    value: Value,
 }
 
 pub struct Table {
-    count: i64,
-    entries: Box<[Rc<Entry>]>,
+    count: usize,
+    entries: Box<Vec<Rc<Entry>>>,
 }
 
 impl Default for Table {
     fn default() -> Self {
        Self {
             count: 0,
-            entries: Box::new([])
+            entries: Box::new(vec![])
        } 
     }
 }
 
 impl Table {
-    fn set(&mut self, key: Vec<char>, value: Value) {
-        self.entries.fill(Rc::new( Entry { key, value }));
+    const MAX_LOAD: f32 = 0.75;
+
+    fn set(&mut self, key: Vec<char>, value: Value) -> bool {
+        if ((self.entries.capacity() - self.count) as f32) < Self::MAX_LOAD {}
+
+
+        if let Some(new_entry) = self.find_entry(&key) {
+            self.entries.fill(new_entry);
+            return true;
+        }
+
+        return false;
+    }
+
+    fn find_entry(&self, key: &Vec<char>) -> Option<Rc<Entry>> {
+
     }
 }
 
 pub fn hash_string(key: &mut Vec<char>) -> Hash {
     let mut hash: Hash = 2166136261;
 
-    for i in 0..key.len() {
-        println!("i is {i}, key is: {}", key[i]);
-        hash ^= key[i].to_digit(36).unwrap();
-        hash = hash.wrapping_mul(16777619);
-    }
+    key.iter().for_each(|&c| {
+        if let Some(digit) = c.to_digit(36) {
+            hash ^= digit;
+            hash = hash.wrapping_mul(16777619);
+        }
+    });
 
     hash
 }

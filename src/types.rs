766fca366fcdbf7482f2e78d03fb9ed57769c138
wnccys <ipooo346@gmail.com>
2024-10-29 19:12:42 -0300

    feat: implements format for table and sets basic test

--- src/types.rs
+++ src/types.rs
@@ -26,11 +26,11 @@ impl Table {
     const MAX_LOAD: f32 = 0.75;
 
     fn set(&mut self, key: Vec<char>, value: Value) -> bool {
-        if ((self.count / self.entries.capacity()) as f32) > Self::MAX_LOAD {
+        if ((self.count + 1 / self.entries.capacity()) as f32) > Self::MAX_LOAD {
             self.entries.reserve((self.count as f32 / Self::MAX_LOAD).ceil() as usize);
         }
 
-        if let Some(new_entry) = self.find_entry(&mut key.to_owned()) {
+        if let Some(new_entry) = self.find_entry(&key) {
             self.entries.fill(Some(new_entry));
             return true;
         }
@@ -38,13 +38,11 @@ impl Table {
         return false;
     }
 
-    fn find_entry(&self, key: &mut Vec<char>) -> Option<Rc<Entry>> {
+    fn find_entry(&self, key: &Vec<char>) -> Option<Rc<Entry>> {
         let mut index = hash_string(key) as usize % self.entries.capacity();
 
         loop {
             let entry = self.entries[index].as_ref().unwrap();
-
-            // match entry.key {}
 
             if entry.key == *key || self.entries[index].is_none() {
                 return Some(Rc::clone(entry));
@@ -55,7 +53,24 @@ impl Table {
     }
 }
 
-pub fn hash_string(key: &mut Vec<char>) -> Hash {
+impl std::fmt::Display for Table {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut all_entries: Vec<(Vec<char>, Value)> = Default::default(); 
+        let mut final_formated: String = Default::default();
+
+        self.entries.iter().for_each(|entry| {
+            all_entries.push((entry.as_ref().unwrap().key.clone(), entry.as_ref().unwrap().value.clone()));
+        });
+
+        all_entries.iter().for_each(|key_value| {
+            final_formated += format!("{:?} => {:?}", key_value.0, key_value.1).as_str();
+        });
+
+        write!(f, "{final_formated}")
+    }
+}
+
+pub fn hash_string(key: &Vec<char>) -> Hash {
     let mut hash: Hash = 2166136261;
 
     key.iter().for_each(|&c| {
@@ -87,9 +102,11 @@ mod tests {
         println!("hash: {}", hash_string(&mut word));
     }
 
+    #[test]
     fn test_find_entry_by_key() {
-        let table = Table::default();
+        let mut table = Table::default();
         let str = vec!['l', 'o', 'l', 'o',' ', 'm', 'e', 'u', ' ', 'a', 'm', 'o', 'r'];
-        table.set();
+        table.set(vec!['a', 'm', 'o', 'r'], Value::String(str));
+        println!("{table}");
     }
 }

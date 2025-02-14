d7f2270aadf44eb7302cf190636553e7fd66b353
wnccys <ipooo346@gmail.com>
2025-02-14 10:10:48 -0300

    doc: set chunk functions summary

--- src/chunk.rs
+++ src/chunk.rs
@@ -31,11 +31,15 @@ pub struct Chunk {
 }
 
 impl Chunk {
+    /// Write OpCode to code vec 
+    /// 
     pub fn write(&mut self, byte: OpCode, line: i32) {
         self.code.push(byte);
         self.lines.push(line);
     }
 
+    /// Write to constants vec
+    /// 
     pub fn write_constant(&mut self, value: Value) -> usize {
         self.constants.push(value);
         self.constants.len() - 1

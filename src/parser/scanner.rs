33ab778fa721435330aa97600035a5658a3f68a5
wnccys <ipooo346@gmail.com>
2025-02-14 10:21:45 -0300

    fix: remove doc trailing space

--- src/parser/scanner.rs
+++ src/parser/scanner.rs
@@ -68,7 +68,7 @@ pub struct Token {
 
 impl Scanner {
     pub fn scan_token(&mut self) -> Token {
-        // NOTE self.start is 0 index based but current is generally always 1 advanced, 
+        // NOTE self.start is 0 index based but current is generally always 1 advanced,
         // so current-1 is the exact position on chars,
         // and self.start..self.start+self.length is a correct range
         // so there's no need to subtract 1 because the index is advanced by 1 already

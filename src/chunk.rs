38cfb4e9b1348a4298643558d8793346748c5b50
wnccys <ipooo346@gmail.com>
2025-02-25 19:13:34 -0300

    doc: improve doc

--- src/chunk.rs
+++ src/chunk.rs
@@ -26,7 +26,7 @@ pub enum OpCode {
 
 #[derive(Debug, Default)]
 pub struct Chunk {
-    /// The sequence of Bytecodes' used to change the stack state.
+    /// The sequence of Bytecodes used to change the stack state.
     pub code: Vec<OpCode>,
     /// Where the Bytecodes' operations itself are executed.
     pub stack: Vec<Value>,
@@ -36,14 +36,14 @@ pub struct Chunk {
 }
 
 impl Chunk {
-    /// Write OpCode to code vec 
+    /// Write to code vec.
     /// 
     pub fn write(&mut self, byte: OpCode, line: i32) {
         self.code.push(byte);
         self.lines.push(line);
     }
 
-    /// Write to constants vec
+    /// Write to constants vec.
     /// 
     pub fn write_constant(&mut self, value: Value) -> usize {
         self.constants.push(value);

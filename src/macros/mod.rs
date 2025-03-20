99a747679443eea157b52a7417cde64a6ad1aa91
wnccys <ipooo346@gmail.com>
2025-03-20 14:24:39 -0300

    feat: generate macros for new Values and Primitives

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,7 +1,62 @@
 /// Generate equal comparison code enabling Primitive(_) == Primitive(_), Primitive(_) > Primitive(_)
 /// 
-macro_rules! gen_primitives_eq_ord {
+macro_rules! gen_primitives_operations {
     ($($variant:ident($inner:ty)), *) => {
+        use std::cmp::Ordering;
+
+        impl Add for Primitive {
+            type Output = Primitive;
+
+            fn add(self, other: Self) -> Primitive {
+                match (self, other) { 
+                    $(
+                        (
+                            Primitive::$variant(value_a),
+                            Primitive::$variant(value_b)
+                        ) => { Primitive::$variant(value_a + value_b) }
+                    ), *
+                    (
+                        Primitive::String(str1), Primitive::String(str2)
+                    ) => {
+                        Primitive::String(str1.add(&str2[..]))
+                    },
+                    _ => panic!("Add not allowed.")
+                }
+            }
+        }
+
+        impl Mul for Primitive {
+            type Output = Primitive;
+
+            fn mul(self, other: Self) -> Primitive {
+                match (self, other) {
+                    $(
+                        (
+                            Primitive::$variant(value_a),
+                            Primitive::$variant(value_b)
+                        ) => { Primitive::$variant(value_a * value_b) }
+                    ), *
+                    _ => panic!("Operation mul not allowed")
+                }
+            }
+        }
+
+        impl Div for Primitive {
+            type Output = Primitive;
+
+            fn div(self, other: Self) -> Primitive {
+                match (self, other) {
+                    $(
+                        (
+                            Primitive::$variant(value_a),
+                            Primitive::$variant(value_b)
+                        ) => { Primitive::$variant(value_a / value_b) }
+                    ), *
+                    _ => panic!("Operation div not allowed")
+                }
+            }
+        }
+
         impl PartialEq for Primitive {
             fn eq(&self, other: &Self) -> bool {
                 match (self, other) {
@@ -31,13 +86,11 @@ macro_rules! gen_primitives_eq_ord {
     };
 }
 
-/// Generate add, mul, and div for it's respective values. 
+/// Generate add, mul, and div for it's respective values.
 /// Strings can only be added; Other implementations are only allowed in numbers (Int and Float)
 /// 
 macro_rules! gen_values_operations {
     ($($variant:ident($inner:ty)), *) => {
-        use std::cmp::Ordering;
-
         impl Add for Value {
             type Output = Value;
 
@@ -45,15 +98,15 @@ macro_rules! gen_values_operations {
                 match (self, other) { 
                     $(
                         (
-                            Value { value: Primitive::$variant(value_a), modifier: Modifier::Mut },
+                            Value { value: Primitive::$variant(value_a), modifier },
                             Value { value: Primitive::$variant(value_b), modifier: _},
-                        ) => { Value { value: Primitive::$variant(value_a + value_b), modifier: Modifier::Mut } }
+                        ) => { Value { value: Primitive::$variant(value_a + value_b), modifier} }
                     ), *
                     (
-                        Value { value: Primitive::String(str1) , modifier: Modifier::Mut },
+                        Value { value: Primitive::String(str1) , modifier },
                         Value { value: Primitive::String(str2), modifier: _ },
                     ) => {
-                        Value { value: Primitive::String(str1.add(&str2[..])), modifier:  Modifier::Mut }
+                        Value { value: Primitive::String(str1.add(&str2[..])), modifier }
                     },
                     _ => panic!("Add not allowed.")
                 }
@@ -67,9 +120,9 @@ macro_rules! gen_values_operations {
                 match (self, other) {
                     $(
                         (
-                            Value { value: Primitive::$variant(value_a), modifier: Modifier::Mut },
+                            Value { value: Primitive::$variant(value_a), modifier },
                             Value { value: Primitive::$variant(value_b), modifier: _},
-                        ) => { Value { value: Primitive::$variant(value_a * value_b), modifier: Modifier::Mut } }
+                        ) => { Value { value: Primitive::$variant(value_a * value_b), modifier  } }
                     ), *
                     _ => panic!("Operation mul not allowed")
                 }
@@ -83,9 +136,9 @@ macro_rules! gen_values_operations {
                 match (self, other) {
                     $(
                         (
-                            Value { value: Primitive::$variant(value_a), modifier: Modifier::Mut },
+                            Value { value: Primitive::$variant(value_a), modifier },
                             Value { value: Primitive::$variant(value_b), modifier: _},
-                        ) => { Value { value: Primitive::$variant(value_a / value_b), modifier: Modifier::Mut } }
+                        ) => { Value { value: Primitive::$variant(value_a / value_b), modifier } }
                     ), *
                     _ => panic!("Operation div not allowed")
                 }
@@ -106,5 +159,5 @@ macro_rules! gen_values_operations {
     }
 }
 
-pub(crate) use gen_primitives_eq_ord;
+pub(crate) use gen_primitives_operations;
 pub(crate) use gen_values_operations;

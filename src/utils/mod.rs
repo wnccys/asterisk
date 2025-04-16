0bc703edac20dd53caf844576b76849cb4ee832d
wnccys <ipooo346@gmail.com>
2025-04-16 13:18:44 -0300

    feat: move parse_type

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,15 @@
+use crate::value::{Primitive, Type};
+
 pub mod print;
+
+/// Parses primitive to Type equivalent
+/// 
+pub fn parse_type(p: &Primitive) -> Type {
+    match p {
+        Primitive::Int(_) => Type::Int,
+        Primitive::Float(_) => Type::Float,
+        Primitive::String(_) => Type::String,
+        Primitive::Bool(_) => Type::Bool,
+        _ => Type::Void,
+    }
+}

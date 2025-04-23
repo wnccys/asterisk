63fd5d7baa835f4ef2e80ac0032300f9ab3b0613
wnccys <ipooo346@gmail.com>
2025-04-22 21:13:38 -0300

    feat: set basic Ref type raw pointer read

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -5,11 +5,13 @@ pub mod print;
 /// Parses primitive to Type equivalent
 ///
 pub fn parse_type(p: &Primitive) -> Type {
+    dbg!(p);
     match p {
         Primitive::Int(_) => Type::Int,
         Primitive::Float(_) => Type::Float,
         Primitive::String(_) => Type::String,
         Primitive::Bool(_) => Type::Bool,
-        _ => Type::Void,
+        Primitive::Ref(t) => Type::Ref(unsafe { &t.read()._type }),
+        _ => panic!("Error parsing type."),
     }
 }

d3b5bf7b1533a2bd7a2aaf63f2088368b6a6fd07
wnccys <ipooo346@gmail.com>
2025-05-14 14:10:29 -0300

    feat: set basic init for function implementation

--- src/compiler.rs
+++ src/compiler.rs
@@ -2,13 +2,17 @@ use crate::chunk::Chunk;
 use crate::parser::scanner::*;
 use crate::parser::Parser;
 use crate::types::hash_table::HashTable;
+use crate::value::Function;
+use crate::value::FunctionType;
 use crate::vm::InterpretResult;
 
-pub fn compile(strings: &mut HashTable<String, String>, source_code: String) -> (Chunk, InterpretResult) {
+pub fn compile(strings: &mut HashTable<String, String>, source_code: String) -> (Function, InterpretResult) {
     let mut source_lines = source_code.lines();
     let mut scanner = Scanner::new(&mut source_lines);
 
-    let mut parser = Parser::new(strings, scanner.scan());
+    let function = Function::default();
+
+    let mut parser = Parser::new(strings, scanner.scan(), function, FunctionType::Script);
     #[cfg(feature = "debug-scan")]
     dbg!(&parser.token_stream);
     parser.advance();

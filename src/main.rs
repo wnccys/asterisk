951fa8638fab0cc31e35e1b891ead0ec66bc2fcb
wnccys <ipooo346@gmail.com>
2025-06-04 14:06:23 -0300

    ref: adjust usage message

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn check_cmd_args(vm: &mut Vm) {
     match args.len() {
         1 => repl(vm),
         2 => run_file(vm, &args[1]),
-        _ => panic!("Usage: ask [path]"),
+        _ => panic!("Usage: cargo run -- [file]"),
     }
 }
 

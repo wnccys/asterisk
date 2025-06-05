ca0eddcc9088660882e9e61dad27306ae9d66c13
wnccys <ipooo346@gmail.com>
2025-06-05 10:32:12 -0300

    feat: set better peek_byte() def

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -52,7 +52,8 @@ impl<R: std::io::Read> Lexer<R> {
 
     fn peek_byte(&mut self) -> &u8 {
         match self.source.peek() {
-            Some(r) => r.as_ref().unwrap(),
+            Some(Ok(r)) => r,
+            Some(_) => panic!("error on byte peek"),
             None => &b'\0',
         }
     }
@@ -167,79 +168,3 @@ pub enum Token {
     Error,
     Eof,
 }
-
-pub const KEYWORDS: LazyLock<HashMap<&'static str, TokenCode>> = LazyLock::new(|| {
-    let mut map: HashMap<&'static str, TokenCode> = HashMap::new();
-
-    // Keywords
-    map.insert("and", TokenCode::And);
-    map.insert("&&", TokenCode::And);
-    map.insert("class", TokenCode::Class);
-    map.insert("case", TokenCode::Case);
-    map.insert("const", TokenCode::Const);
-    map.insert("continue", TokenCode::Continue);
-    map.insert("default", TokenCode::Default);
-    map.insert("else", TokenCode::Else);
-    map.insert("false", TokenCode::False);
-    map.insert("for", TokenCode::For);
-    map.insert("fn", TokenCode::Fun);
-    map.insert("if", TokenCode::If);
-    map.insert("mut", TokenCode::Modifier);
-    map.insert("number", TokenCode::Number);
-    map.insert("nil", TokenCode::Nil);
-    map.insert("or", TokenCode::Or);
-    map.insert("||", TokenCode::Or);
-    map.insert("print", TokenCode::Print);
-    map.insert("return", TokenCode::Return);
-    map.insert("switch", TokenCode::Switch);
-    map.insert("super", TokenCode::Super);
-    map.insert("this", TokenCode::This);
-    map.insert("true", TokenCode::True);
-    map.insert("let", TokenCode::Var);
-    map.insert("while", TokenCode::While);
-
-    // Punctuation and operators
-    map.insert("(", TokenCode::LeftParen);
-    map.insert(")", TokenCode::RightParen);
-    map.insert("{", TokenCode::LeftBrace);
-    map.insert("}", TokenCode::RightBrace);
-    map.insert(";", TokenCode::SemiColon);
-    map.insert(":", TokenCode::Colon);
-    map.insert(",", TokenCode::Comma);
-    map.insert(".", TokenCode::Dot);
-    map.insert("+", TokenCode::Plus);
-    map.insert("-", TokenCode::Minus);
-    map.insert("*", TokenCode::Star);
-    map.insert("/", TokenCode::Slash);
-    map.insert("&", TokenCode::Ampersand);
-    map.insert("//", TokenCode::Comment);
-    map.insert("!", TokenCode::Bang);
-    map.insert("!=", TokenCode::BangEqual);
-    map.insert("=", TokenCode::Equal);
-    map.insert("==", TokenCode::EqualEqual);
-    map.insert("<", TokenCode::Less);
-    map.insert("<=", TokenCode::LessEqual);
-    map.insert(">", TokenCode::Greater);
-    map.insert(">=", TokenCode::GreaterEqual);
-
-    /// Generate Primitive and it's Ref Type
-    ///
-    macro_rules! gen_types_n_refs {
-        ($($variant:ident),* $(,)?) => {
-            use std::rc::Rc;
-            {
-                $(
-                    map.insert(stringify!($variant), TokenCode::TypeDef(Type::$variant));
-                    map.insert(concat!("&", stringify!($variant)), TokenCode::TypeDef(Type::Ref(Rc::new(Type::$variant))));
-                )*
-            }
-        }
-    }
-
-    gen_types_n_refs!(Int, Float, String, Bool, Void);
-
-    // General compiler track
-    map.insert("EOF", TokenCode::Eof);
-
-    map
-});

7613f8bb2c95786b97e1950f71fce91503a8c8df
wnccys <ipooo346@gmail.com>
2025-06-11 14:43:18 -0300

    WIP: setting up parser error def

--- src/errors/parser_errors.rs
+++ src/errors/parser_errors.rs
@@ -2,10 +2,15 @@ use std::{error, fmt};
 
 pub type ParserResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 
-pub struct xError;
+#[derive(Debug)]
+pub struct xError {
+    message: &'static str
+}
 
 impl fmt::Display for xError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Error")
     }
 }
+
+impl error::Error for xError {}

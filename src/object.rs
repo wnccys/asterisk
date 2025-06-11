6647a371ed7ac8e0483251d164b3bcc387db98f6
wnccys <ipooo346@gmail.com>
2025-06-11 10:50:23 -0300

    feat: entirelly remake NativeFn

--- src/object.rs
+++ src/object.rs
@@ -1,12 +1,17 @@
-use std::{any::Any, env::Args};
-//
-// pub struct NativeFn {
-//     pub arity: u32,
-//     _fn: dyn Fn() -> ()
-// }
-//
-// impl NativeFn {
-//     fn call(&mut self) {
-//         (self._fn.())
-//     }
-// }
+use std::{any::Any, cell::RefCell, env::Args, rc::Rc};
+
+use crate::value::Value;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct NativeFn {
+    pub arity: usize,
+    pub _fn: fn(&[Rc<RefCell<Value>>]) -> Value
+}
+
+impl NativeFn {
+    pub fn call(&mut self, args: &[Rc<RefCell<Value>>]) -> Value {
+        if args.len() != self.arity { panic!("Expect {} but got {} arguments.", args.len(), self.arity) }
+
+        (self._fn)(args)
+    }
+}

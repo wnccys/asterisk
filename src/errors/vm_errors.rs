dc5600f1fd874c67511e7b18284256b8c72bd004
wnccys <ipooo346@gmail.com>
2025-06-11 14:43:03 -0300

    feat: set VM basic error def

--- src/errors/vm_errors.rs
+++ src/errors/vm_errors.rs
@@ -3,12 +3,26 @@ use std::{error, fmt};
 pub type VmResult<T> = Result<T, Box<dyn error::Error>>;
 
 #[derive(Debug)]
+pub struct InterpretError {
+    message: &'static str,
+    _type: InterpretResult
+}
+
+
+#[derive(Debug)]
 pub enum InterpretResult {
-    Ok,
     RuntimeError,
     CompilerError,
 }
 
-impl fmt::Display for InterpretResult {
-    fn fmt() {}
+impl fmt::Display for InterpretError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self._type {
+            InterpretResult::RuntimeError => write!(f, "Runtime error: {}", self.message),
+            InterpretResult::CompilerError => write!(f, "An compilation error occurred: {}", self.message),
+        }
+    }
 }
+
+impl error::Error for InterpretError {}
+

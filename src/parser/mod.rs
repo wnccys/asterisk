207cce8693b79e74b12759c8174b3f3c43aad3c7
wnccys <ipooo346@gmail.com>
2025-07-11 14:03:04 -0300

    feat(test): add multi argument fn test

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -807,9 +807,29 @@ mod tests {
         assert_eq!(f.name, "f");
     }
 
+    #[test]
     fn fun_declaration_multi_argument() {
         let source = r"
-            fn fun (n: Int, m: String) {}
+            fn g(n: Int, m: String, p: &Int, g: &String, b: Bool, c: Float, d: &Float) {}
         ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.fun_declaration();
+
+        // Extract function from current parser's chunk
+        let _fn = parser
+            .function
+            .chunk
+            .constants
+            .get(1).unwrap_or_else(|| panic!("Could not find function object"));
+
+        let f = match _fn { 
+            Primitive::Function(f) => f,
+            f => panic!("{}", format!("Invalid function object: {:?}", f))
+        };
+
+        assert_eq!(f.arity, 7);
+        assert_eq!(f.name, "g");
     }
 }

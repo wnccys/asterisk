f20ad925b26d4253ee364dcdf88deb8feb9b9665
wnccys <ipooo346@gmail.com>
2025-07-11 11:56:31 -0300

    tests/fix: adjust lexing and set basic test

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -97,6 +97,13 @@ impl<R: std::io::Read> Lexer<R> {
             b':' => Token::Colon,
             b';' => Token::SemiColon,
             b'*' => Token::Star,
+            b'|' => {
+                if self.read_byte() != b'|' {
+                    return Token::Error("Invalid '|'")
+                }
+
+                Token::Or
+            }
             b'&' => self.check_ahead(b'&', Token::Ampersand, Token::And),
             b'!' => self.check_ahead(b'=', Token::Bang, Token::BangEqual),
             b'=' => self.check_ahead(b'=', Token::Equal, Token::EqualEqual),
@@ -143,6 +150,7 @@ impl<R: std::io::Read> Lexer<R> {
 
     fn check_ahead(&mut self, ahead: u8, short: Token, long: Token) -> Token {
         if *self.peek_byte() == ahead {
+            self.next_byte();
             long
         } else {
             short
@@ -307,6 +315,7 @@ impl<R: std::io::Read> Lexer<R> {
             "switch" => Token::Switch,
             "super" => Token::Super,
             "this" => Token::This,
+            "true" => Token::True,
             "let" => Token::Var,
             "while" => Token::While,
             "Int" => Token::TypeDef(Type::Int),
@@ -364,5 +373,88 @@ impl<R: std::io::Read> Lexer<R> {
 
     fn error(&mut self) {
 
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Lexer, Token, Type};
+
+    #[test]
+    pub fn word_matching() {
+        /* 
+            Omitted: Nil and Error 
+            Comment (//, /* */) are present on source, but it is skipped naturally by lexer
+        */
+        let tokens_mock: [Token; 48] = [
+            Token::LeftParen,
+            Token::RightParen,
+            Token::LeftBrace,
+            Token::RightBrace,
+            Token::Comma,
+            Token::Dot,
+            Token::Minus,
+            Token::Plus,
+            Token::Colon,
+            Token::SemiColon,
+            Token::Slash,
+            Token::Star,
+            Token::Ampersand,
+            Token::Bang,
+            Token::BangEqual,
+            Token::Equal,
+            Token::EqualEqual,
+            Token::Greater,
+            Token::GreaterEqual,
+            Token::Less,
+            Token::LessEqual,
+            Token::Identifier(String::from("ident")),
+            Token::String("str".as_bytes().to_vec()),
+            Token::Float(1.0),
+            Token::Integer(1),
+            Token::And,
+            Token::Class,
+            Token::Case,
+            Token::Const,
+            Token::Continue,
+            Token::Default,
+            Token::Else,
+            Token::False,
+            Token::For,
+            Token::Fun,
+            Token::If,
+            Token::Modifier,
+            Token::TypeDef(Type::String),
+            Token::Or,
+            Token::Print,
+            Token::Return,
+            Token::Switch,
+            Token::Super,
+            Token::This,
+            Token::True,
+            Token::Var,
+            Token::While,
+            Token::Eof,
+        ];
+
+        let source = r"
+            ( ) { } , . - + : ; / * &
+            ! != = == > >= < <=
+            ident 'str' 1.0 1
+            && class case const continue
+            default else false for fn if
+            mut String || print return switch
+            super this true let while // /* */ \0
+        ";
+
+        let mut lex = 
+            Lexer::new(
+                std::io::Cursor::new(source)
+            );
+
+        for t in tokens_mock.into_iter() {
+            let l_tok = lex.next();
+            if t != l_tok { panic!("Missmatched token: {t:?} with {l_tok:?}") }
+        }
     }
 }

0b93fdb0ef6d7517cd2c14d487a4d075a7fa4b86
wnccys <ipooo346@gmail.com>
2025-07-14 14:36:41 -0300

    tests: set immut var assign

--- tests/variables.rs
+++ tests/variables.rs
@@ -3,17 +3,24 @@ use common::mk_parser;
 
 #[cfg(test)]
 pub mod variables {
+    use asterisk::{primitives::primitive::Primitive, vm::Vm};
+
     use super::*;
-    use std::io::Cursor;
+    use std::{io::Cursor, panic::{catch_unwind, AssertUnwindSafe}, rc::Rc};
 
     #[test]
     fn var_declaration_immut() {
-        let source = r"
-            let a = 32;
-            a = 2;
-        ";
+        let mut vm = Vm::default();
+        let sources: [&'static str; 2] = [
+            r"
+                let a = 32; // 1
+            ",
+            r"
+                a = 2; // 2
+            "
+        ];
 
-        let mut parser = mk_parser(Cursor::new(source));
+        let mut parser = mk_parser(Cursor::new(sources[0]));
         parser.advance();
         parser.var_declaration();
 
@@ -22,7 +29,31 @@ pub mod variables {
         // No locals were added
         assert_eq!(parser.scopes.len(), 0);
 
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        let _ = vm.run();
+
+        let var_value = match &vm.globals
+            .get(&"a".to_string())
+            .expect("Variable is not available on VM.")
+            .borrow().value {
+                Primitive::Int(i) => *i,
+                _ => panic!("Could not find variable by name.")
+            };
+
+        assert_eq!(var_value, 32);
+
+        let mut parser = mk_parser(Cursor::new(sources[1]));
         parser.statement();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+
+        let r = catch_unwind(
+            AssertUnwindSafe(|| {
+                let _ = vm.run();
+            }
+        ));
+
+        assert!(r.is_err());
     }
 
     #[test]

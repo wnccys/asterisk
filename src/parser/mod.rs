f9949b899ff2f0db0ff60f666ab74bd6eb3fbc4b
wnccys <ipooo346@gmail.com>
2025-07-14 08:27:52 -0300

    fix/tests: set correct char info on block error and set local scope test

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -866,4 +866,49 @@ mod tests {
         // No locals was added
         assert_eq!(parser.scopes.len(), 0);
     }
+
+    #[test]
+    fn var_declaration_immut_local() {
+        let source = r"
+            {
+                let a = 32;
+                a = 2;
+            }
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.advance();
+        parser.block();
+        // parser.var_declaration();
+
+        // "a" and "32"
+        assert_eq!(parser.function.chunk.constants.len(), 2);
+        // No locals were added
+        assert_eq!(parser.scopes.len(), 1);
+
+        // parser.statement();
+    }
+
+    #[test]
+    fn var_declaration_mut_local() {
+        let source = r"
+            {
+                let mut a = 32;
+                a = 2;
+            }
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.advance();
+        parser.begin_scope();
+        parser.block();
+        // Block are not uninitialized
+
+        // "a" and "32"
+        assert_eq!(parser.function.chunk.constants.len(), 2);
+        // No locals was added
+        assert_eq!(parser.scopes.len(), 1);
+    }
 }

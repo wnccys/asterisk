61101d3f39c7bd3532513e47d23cd3bca84a9bce
wnccys <ipooo346@gmail.com>
2025-07-17 14:50:18 -0300

    test: set mul float op test

--- tests/ops.rs
+++ tests/ops.rs
@@ -159,6 +159,56 @@ mod ops {
         assert_eq!(result, -9);
     }
 
+    #[test]
+    fn mul_float_positive() {
+        let mut vm = Vm::default();
+        let a = Value {
+            value: Primitive::Float(2.0),
+            ..Default::default()
+        };
+
+        let b = Value {
+            value: Primitive::Float(5.0),
+            ..Default::default()
+        };
+        vm.stack.push(Rc::new(RefCell::new(a)));
+        vm.stack.push(Rc::new(RefCell::new(b)));
+
+        let _ = vm.binary_op("*");
+
+        let result = match vm.stack.pop().unwrap().borrow().value {
+            Primitive::Float(f) => f,
+            _ => panic!("Invalid output result.")
+        };
+
+        assert_eq!(result, 10.0);
+    }
+
+    #[test]
+    fn mul_float_negative() {
+        let mut vm = Vm::default();
+        let a = Value {
+            value: Primitive::Float(2.0),
+            ..Default::default()
+        };
+
+        let b = Value {
+            value: Primitive::Float(-5.0),
+            ..Default::default()
+        };
+        vm.stack.push(Rc::new(RefCell::new(a)));
+        vm.stack.push(Rc::new(RefCell::new(b)));
+
+        let _ = vm.binary_op("*");
+
+        let result = match vm.stack.pop().unwrap().borrow().value {
+            Primitive::Float(f) => f,
+            _ => panic!("Invalid output result.")
+        };
+
+        assert_eq!(result, -10.0);
+    }
+
     fn div() {
 
     }

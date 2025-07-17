1346e12bf4c895f3895ab86d5523b9620fe01206
wnccys <ipooo346@gmail.com>
2025-07-17 08:49:57 -0300

    tests: set immut local

--- tests/variables.rs
+++ tests/variables.rs
@@ -110,6 +110,35 @@ pub mod variables {
     }
 
     #[test]
+    fn var_declaration_immut_local() {
+        let mut vm = Vm::default();
+        let source = r"
+            {
+                let a = 32;
+                a = 2;
+            }
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.begin_scope();
+        parser.block();
+
+        assert_eq!(parser.scopes.len(), 1);
+        parser.end_scope();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        let result = catch_unwind(AssertUnwindSafe(|| {
+            match vm.run() {
+                Ok(_) => (),
+                Err(e) => { panic!("{:?}", e)}
+            }
+        }));
+
+        assert!(result.is_err());
+    }
+
+    #[test]
     fn var_declaration_mut_local() {
         let source = r"
             {

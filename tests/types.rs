23af7af332832c53d5ba7caa99b025fdd58a6cef
wnccys <ipooo346@gmail.com>
2025-10-09 09:08:16 -0300

    feat: set new primitive type test

--- tests/types.rs
+++ tests/types.rs
@@ -9,7 +9,151 @@ mod types {
     use asterisk::{primitives::types::Type, vm::Vm};
 
     #[test]
-    fn functions() {
+    fn implicit_int() {
+        let mut vm = Vm::default();
+        let source = r"
+            let a = 32;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.var_declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        assert_eq!(a.borrow()._type, Type::Int);
+    }
+
+    #[test]
+    fn explicit_int() {
+        let mut vm = Vm::default();
+        let source = r"
+            let a: Int = 32;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.var_declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        assert_eq!(a.borrow()._type, Type::Int);
+    }
+
+    #[test]
+    fn implicit_float() {
+        let mut vm = Vm::default();
+        let source = r"
+            let a = 32.0;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.var_declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        assert_eq!(a.borrow()._type, Type::Float);
+    }
+
+    #[test]
+    fn explicit_float() {
+        let mut vm = Vm::default();
+        let source = r"
+            let a: Float = 10.5;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.var_declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        assert_eq!(a.borrow()._type, Type::Float);
+    }
+    
+    #[test]
+     fn implicit_bool() {
+        let mut vm = Vm::default();
+        let source = r"
+            let a = false;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.var_declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        assert_eq!(a.borrow()._type, Type::Bool);
+     }
+
+     #[test]
+     fn explicit_bool() {
+        let mut vm = Vm::default();
+        let source = r"
+            let a: Bool = true;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.var_declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        assert_eq!(a.borrow()._type, Type::Bool);
+     }
+
+     #[test]
+     fn implicit_string() {
+        let mut vm = Vm::default();
+        let source = r"
+            let a = 't';
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.var_declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        assert_eq!(a.borrow()._type, Type::String);
+     }
+
+     #[test]
+     fn explicit_string() {
+        let mut vm = Vm::default();
+        let source = r"
+            let a: String = 'xyz';
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser.var_declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        assert_eq!(a.borrow()._type, Type::String);
+     }
+
+    #[test]
+    fn function() {
         let mut vm = Vm::default();
         let source = r"
             fn f() {}
@@ -31,7 +175,25 @@ mod types {
     }
 
     #[test]
-    fn closures() {
+    fn simple_closure() {
+        let mut vm = Vm::default();
+        let source = r"
+            let a = fn() {};
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        parser.advance();
+        parser = parser.fun_declaration();
+
+        parser.advance();
+        parser.var_declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        let _ = vm.run().unwrap();
+    }
+
+    #[test]
+    fn complex_closure() {
         let mut vm = Vm::default();
         let source = r"
             fn f() {
@@ -51,7 +213,7 @@ mod types {
         parser.var_declaration();
 
         vm.call(Rc::new(parser.end_compiler()), 0);
-        let _ = vm.run();
+        let _ = vm.run().unwrap();
 
         let a = vm.globals.get(&String::from("a")).unwrap();
         assert_eq!(a.borrow()._type, Type::Closure);

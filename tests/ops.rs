116611e65b78d8edb0af7806c9857f6c6b2f6fa4
wnccys <ipooo346@gmail.com>
2025-10-10 09:31:18 -0300

    feat: set better bool coverage

--- tests/ops.rs
+++ tests/ops.rs
@@ -3,11 +3,11 @@ use common::mk_parser;
 
 #[cfg(test)]
 mod ops {
-    use std::{cell::RefCell, rc::Rc};
+    use std::{cell::RefCell, io::Cursor, rc::Rc};
 
-    use asterisk::{primitives::{primitive::Primitive, types::Type, value::Value}, vm::Vm};
+    use asterisk::{primitives::{primitive::Primitive, value::Value}, vm::Vm};
 
-    use super::*;
+    use crate::common::mk_parser;
 
     #[test]
     fn sum_int_positive() {
@@ -307,5 +307,91 @@ mod ops {
         };
 
         assert_eq!(result, -4.0);
+    }
+
+    /* Bool conditional ops */
+
+    #[test]
+    fn bool_or() {
+        let mut vm = Vm::default();
+        let source = r"
+            let mut a = true or false;
+            let mut b = true || false;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        // var decl
+        parser = parser.declaration();
+        parser = parser.declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        let b = vm.globals.get(&"b".to_string()).unwrap();
+
+        assert_eq!(a.borrow().value, Primitive::Bool(true));
+        assert_eq!(b.borrow().value, Primitive::Bool(true));
+    }
+
+    #[test]
+    fn bool_and() {
+        let mut vm = Vm::default();
+        let source = r"
+            let mut a = true and false;
+            let mut b = true && false;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        // var decl
+        parser = parser.declaration();
+        parser = parser.declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+        let b = vm.globals.get(&"b".to_string()).unwrap();
+
+        assert_eq!(a.borrow().value, Primitive::Bool(false));
+        assert_eq!(b.borrow().value, Primitive::Bool(false));
+    }
+
+    #[test]
+    fn bool_negation_true_to_false() {
+        let mut vm = Vm::default();
+        let source = r"
+            let mut a = !true;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        // var decl
+        parser = parser.declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+
+        assert_eq!(a.borrow().value, Primitive::Bool(false));
+    }
+
+    #[test]
+    fn bool_negation_false_to_true() {
+        let mut vm = Vm::default();
+        let source = r"
+            let mut a = !false;
+        ";
+
+        let mut parser = mk_parser(Cursor::new(source));
+        // var decl
+        parser = parser.declaration();
+
+        vm.call(Rc::new(parser.end_compiler()), 0);
+        vm.run().unwrap();
+
+        let a = vm.globals.get(&"a".to_string()).unwrap();
+
+        assert_eq!(a.borrow().value, Primitive::Bool(true));
     }
 }

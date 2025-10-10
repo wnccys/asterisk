e3c1722a96d4cfaca36388dfbd757acd61d9a483
wnccys <ipooo346@gmail.com>
2025-10-10 13:25:08 -0300

    feat: set better eq test

--- tests/ops/equal.rs
+++ tests/ops/equal.rs
@@ -16,32 +16,38 @@ mod equal {
             let b2 = 2 == 1;
             let c1 = 2.0 == 2.0;
             let c2 = 2.0 == 3.0;
+            let d1 = true == true;
+            let d2 = true == false;
         ";
 
         let mut parser = mk_parser(Cursor::new(source));
-        // var decl
-        parser = parser.declaration();
-        parser = parser.declaration();
-        parser = parser.declaration();
-        parser = parser.declaration();
-        parser = parser.declaration();
-        parser = parser.declaration();
+
+        const QNTT: usize = 8;
+
+        for _ in 0..QNTT {
+            // var decl
+            parser = parser.declaration();
+        }
 
         vm.call(Rc::new(parser.end_compiler()), 0);
         vm.run().unwrap();
 
-        let a1 = vm.globals.get(&"a1".to_string()).unwrap().take();
-        let a2 = vm.globals.get(&"a2".to_string()).unwrap().take();
-        let b1 = vm.globals.get(&"b1".to_string()).unwrap().take();
-        let b2 = vm.globals.get(&"b2".to_string()).unwrap().take();
-        let c1 = vm.globals.get(&"c1".to_string()).unwrap().take();
-        let c2 = vm.globals.get(&"c2".to_string()).unwrap().take();
+        let names: [&str; QNTT] = [
+            "a1", "a2",
+            "b1", "b2",
+            "c1", "c2",
+            "d1", "d2"
+        ];
 
-        assert_eq!(a1.value, Primitive::Bool(true));
-        assert_eq!(a2.value, Primitive::Bool(false));
-        assert_eq!(b1.value, Primitive::Bool(true));
-        assert_eq!(b2.value, Primitive::Bool(false));
-        assert_eq!(c1.value, Primitive::Bool(true));
-        assert_eq!(c2.value, Primitive::Bool(false));
+        for i in 0..QNTT {
+            let it = vm.globals.get(&names[i].to_string()).unwrap().take();
+
+            let cond = match i % 2 {
+                1 => false,
+                _ => true
+            };
+
+            assert_eq!(it.value, Primitive::Bool(cond));
+        }
     }
 }

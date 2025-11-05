cd3960de4dcadecfbd1d35beac01e9309cf756b9
wnccys <ipooo346@gmail.com>
2025-11-05 13:32:02 -0300

    feat: set Dyn struct def

--- src/primitives/types.rs
+++ src/primitives/types.rs
@@ -1,6 +1,8 @@
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc};
 
-/* Primitives are variable assigned data, Type are the contract for this data to be valid throught the runtime */
+use crate::primitives::value::Value;
+
+/* Primitives are variable assigned data, Type is the contract for this data to be valid throught the runtime */
 #[derive(Default, Debug, Clone, PartialEq)]
 pub enum Type {
     Float,
@@ -10,13 +12,19 @@ pub enum Type {
     Struct,
     Tuple,
     Fn,
+    // Dyn is resolved dynamically entirelly at VM's bytecode execution phase
+    Dyn(Dyn),
     NativeFn,
     Closure,
     Ref(Rc<Type>),
+    Void,
     #[default]
     UnInit,
-    Void,
 }
+
+#[derive(Default, Debug, Clone, PartialEq)]
+// type struct's (name, type)
+pub struct Dyn(pub Rc<RefCell<Value>>);
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Modifier {
